b820728483320874a3b06a946a4d47811ffdc2a7
Dániel Kántor <git@daniel-kantor.com>
2021-12-02 17:18:42 +0100

    fix: fix faulty directory import

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import getColorschemeSnapshot from ".";
+import getColorschemeSnapshot from "./index.js";
 
 getColorschemeSnapshot(process.argv[2], {}).then(data => {
 	process.stdout.write(data)
