8cc9a422448a3708f0bc0520d86bdc3509a5b14f
Dániel Kántor <git@daniel-kantor.com>
2021-12-02 15:32:15 +0100

    feat: cache data to avoid frequent API calls

--- index.js
+++ index.js
@@ -4,20 +4,43 @@ import fetch from 'node-fetch'
 import { fileURLToPath } from 'url';
 import Mustache from 'mustache'
 import moment from 'moment'
+import cache from 'persistent-cache'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const BACKEND_URL = "http://localhost:8080/"
+const DATA_CACHE = cache();
 
 const getTemplate = application =>
 	fs.readFile(path.resolve(__dirname, 'templates', `${application}.mustache`), 'UTF-8')
 
-const getThemevariables = async settings => {
+const updateThemeData = async () => {
+	const lastFetch = moment.unix(DATA_CACHE.getSync('lastFetch'))
+	if (lastFetch.subtract(24, 'hours').isBefore(moment())) {
+		return null
+	}
 	const themeDataResponse = await fetch(BACKEND_URL)
 	const themeDataResponseJSON = await themeDataResponse.json()
 
 	const { themeData } = themeDataResponseJSON
 	const { timeline, themeVariables } = themeData
+	DATA_CACHE.putSync("lastFetch", moment().unix())
+	DATA_CACHE.putSync("timeline", timeline)
+	DATA_CACHE.putSync("themeVariables", themeVariables)
+
+	return null
+}
+
+const getThemeData = async () => {
+	await updateThemeData()
+	const timeline = DATA_CACHE.getSync('timeline')
+	const themeVariables = DATA_CACHE.getSync('themeVariables')
+	
+	return { timeline, themeVariables }
+}
+
+const getThemeVariables = async settings => {
+	const { timeline, themeVariables } = await getThemeData()
 
 	const nonPastTimelineItems = timeline
 		.filter(([timestamp, _]) => {
@@ -32,7 +55,7 @@ const getThemevariables = async settings => {
 }
 
 export default async function getColorschemeSnapshot(application, settings) {
-	const themeVariables = await getThemevariables()
+	const themeVariables = await getThemeVariables()
 	const template = await getTemplate(application)
 	
 
