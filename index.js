4b790e85f4c72d05f2cafde2122910508630d03a
Dániel Kántor <git@daniel-kantor.com>
2021-12-03 11:08:32 +0100

    refactor: use cross-fetch instead of isomorphic-fetch

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 import Mustache from 'mustache'
 import moment from 'moment'
 import cache from 'persistent-cache'
-import 'isomorphic-fetch';
+import fetch from 'cross-fetch';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
