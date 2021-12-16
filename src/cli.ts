afa7e8160d682ae519364e0ec97f484ce56c4d75
Dániel Kántor <git@daniel-kantor.com>
2021-12-16 14:55:27 +0100

    chore: set up eslint

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import getColorschemeSnapshot from "./index.js";
+import getColorschemeSnapshot from './index.js'
 
 getColorschemeSnapshot(process.argv[2], {}).then(data => {
-	process.stdout.write(data)
+    process.stdout.write(data)
 })
