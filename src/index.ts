f9fd92cb88c574c8175e28d5a73adec0fc5b19c2
Dániel Kántor <git@daniel-kantor.com>
2021-12-29 15:11:31 +0100

    fix: fix error tracking config

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import * as Sentry from '@sentry/node'
 import '@sentry/tracing'
 
 Sentry.init({
-  dsn: 'https://e82ffd8b2fe94d2fa7776295dda8388f@o1091251.ingest.sentry.io/6107984',
+  dsn: 'https://530d556fe4f94451b35d0f2631175fac@o1091251.ingest.sentry.io/6110593',
   tracesSampleRate: 1.0,
 })
 
